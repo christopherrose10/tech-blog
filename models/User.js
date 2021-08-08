ddaf8f5cae533fc17ed97b18bde7174d1f6ff9b9
UA CR <blazer5@email.arizona.edu>
2021-08-08 16:17:22 -0700

    hash password and verify user

--- models/User.js
+++ models/User.js
@@ -1,24 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-// create our User model
-class User extends Model { }
+// create user model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 User.init(
     {
-        // define an id column
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
-        // define a username column
         username: {
             type: DataTypes.STRING,
             allowNull: false
         },
-        // define an email column
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -27,7 +30,6 @@ User.init(
                 isEmail: true
             }
         },
-        // define a password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -37,6 +39,18 @@ User.init(
         }
     },
     {
+        hooks: {
+
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
@@ -44,5 +58,4 @@ User.init(
         modelName: 'user'
     }
 );
-
 module.exports = User;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,20 +49,46 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        res.json({ user: dbUserData });
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'Logged In' });
+
+    });
+})
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
+
     User.update(req.body, {
+        individualHooks: true,
         where: {
             id: req.params.id
         }
+    }).then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
     })
-        .then(dbUserData => {
-            if (!dbUserData[0]) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
