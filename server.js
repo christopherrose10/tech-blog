33e02207255aaa6c0ff4f183696d48fe646f5b6d
UA CR <blazer5@email.arizona.edu>
2021-08-08 22:43:26 -0700

    installed express session library

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helper');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
